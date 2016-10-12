14eb5cd03df67f5d42c99332f29a5e67535e7631
taojunqing <taojun_qing@163.com>
2016-10-12 16:35:19 +0800

    Initial commit

---
+++ jpetservice/src/main/java/com/joypay/JpetServiceApplication.java
@@ -0,0 +1,12 @@
+package com.joypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpetServiceApplication.class, args);
+	}
+}
---
+++ regcenter/src/test/java/com/joypay/RegCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joypay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
